37ceb843c0c95ce4f5a67c3abd529997836fb599
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-09-12 10:16:16 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+  i = 0
+  my_collection = Array.new
+  while collection.length > 0
+    my_collection << yield(collection[i])
+    i += 1
+  end
+  my_collection
+end
